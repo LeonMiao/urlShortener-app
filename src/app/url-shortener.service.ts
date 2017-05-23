8499cdd3ef74b1cc2fd825b21786203a01166087
Leon Miao <miaos@seattleu.edu>
2017-05-23 12:09:50 -0700

    added shortUrl post, but only used mock calls instead of creating data by real conversion methods

--- src/app/url-shortener.service.ts
+++ src/app/url-shortener.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -18,6 +18,11 @@ export class UrlShortenerService {
       .map(response => response.json());
   }
 
-  
+  convertAndStore(inputLongUrl: string) {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
 
+    return this.http.post(this.host + '/app/url/', {longUrl: inputLongUrl})
+      .map(response => response.json());
+  }
 }
